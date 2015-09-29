5829586c05b2a73183f0c6f54655ddb4cb0afbc2
Jernej Lipovec <neykomir@gmail.com>
2015-09-29 15:22:37 +0200

    favicon correction

--- resources/views/elements/head.blade.php
+++ resources/views/elements/head.blade.php
@@ -18,4 +18,4 @@
 
 <link href='http://fonts.googleapis.com/css?family=Lato:300,400,700,900,300italic,400italic,700italic,900italic' rel='stylesheet' type='text/css' />
 <link rel="stylesheet" href="/css/style.css">
-<link rel="shortcut icon" href="favicon.ico" />
+<link rel="shortcut icon" href="/favicon.ico" />
